9c3ad0f73b8aa8050262eb7c1d25247ab44da302
MOARMOARMAN <charleswang439@gmail.com>
2026-07-23 03:24:49 +0800

    Implemented the simultaneous launching of both FastAPI endpoints and react dashboard

--- inventory_dashboard.py
+++ inventory_dashboard.py
@@ -4,8 +4,12 @@ import time
 import atexit
 import logging
 import sys
+from pathlib import Path
+from subprocess import Popen
 
 logger = logging.getLogger("Inventory Dashboard")
+
+ROOT = Path(__file__).resolve().parent
 
 def close_processes(backend_process, frontend_process):
     logger.info("Closing Inventory Dashboard Services.")
@@ -14,10 +18,24 @@ def close_processes(backend_process, frontend_process):
 
 def launch_processes():
     api_process = subprocess.Popen(
-        [sys.executable, "-m", "unvicorn", "inventory:app", "--port", "8000"]
+        [sys.executable, "-m", "uvicorn", "backend.inventory:app", "--port", "8000"],
+        cwd = ROOT
     )
     dashboard_process = subprocess.Popen(
-
+        ["npm.cmd", "run", "dev"],
+        cwd = ROOT / "frontend"
     )
 
     atexit.register(close_processes, api_process, dashboard_process)
+
+    time.sleep(3)
+    webbrowser.open_new_tab("http://localhost:5173")
+
+    try:
+        api_process.wait()
+    except KeyboardInterrupt:
+        print("\nStopping dashboard...")
+
+if __name__ == "__main__":
+    launch_processes()
+
