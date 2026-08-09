96f0f47dec05af85c76eb91f7ca07dcd59349da2
MOARMOARMAN <charleswang439@gmail.com>
2026-08-10 00:00:46 +0800

    Quick fix

--- backend/batch.py
+++ backend/batch.py
@@ -99,7 +99,7 @@ def analyze_batch_overpay_loop() -> None:
                 purchase_link = f"https://steamcommunity.com/market/listings/730/{skin_code}?detail={listing_ID}"
                 message = ""
                 message += f"\n{count}:\n    Link to Purchase: {purchase_link}"
-                message += f"\nFloat: {data.get("float")}"
+                message += f"\nFloat: {data.get("float_val")}"
                 message += f"\nPrice: {data.get("price")}"
                 message += f"\nPrice_deviation: {data.get("overpay_percentage")}%\n"
                 discord_notification(message)
